03a2119f59b17b2c71a0bbb3836067139bd14d20
SUSHANTHIP <88308613+SUSHANTHIP@users.noreply.github.com>
2021-08-20 08:53:38 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,22 @@
+<script>
+        function validateform() 
+        {
+            var email = document.regform.email.value;
+            if (email == null || email == " ") {
+                alert("Email cannot be blank");
+            }
+                return false;
+           } 
+        
+    
+        function validateform()
+        {
+            var email=document.regform .email .value;
+            if(email==null|| email==" ")
+            {
+                alert("Email cannot be blank");
+            }
+                return false;
+         
+        }
+    </script>
